6196b75ab975d976c3999d0da8ca117d82775fa1
Jerome Fayot <jfa@Jeromes-MacBook-Pro.local>
2026-06-22 07:59:04 +0200

    build: updated dependencies

--- examples/react/vite.config.ts
+++ examples/react/vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig, PreviewServer } from "vite";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { cesiumEngine } from "vite-plugin-cesium-engine";
 
